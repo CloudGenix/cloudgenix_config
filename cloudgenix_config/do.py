bae1142c1cbe33fc5fce2c6e744d097215473a3e
Vishwas P G <vpg@paloaltonetworks.com>
2023-07-19 10:00:56 +0530

    Fix for CGBL-1436. For spoke_ha_config

--- cloudgenix_config/do.py
+++ cloudgenix_config/do.py
@@ -1629,13 +1629,16 @@ def handle_element_spoke_ha(matching_element, site_id, config_element, interface
         elem_template['cluster_id'] = hubclusters_n2id.get(elem_template['cluster_id'])
 
     # Check for changes in cleaned config copy and cleaned template (will finally detect spoke HA changes here):
-    if spoke_ha_config and reset_spoke_ha and element_change_check.get('spoke_ha_config'):
-        if elem_template.get('spoke_ha_config', {}).get('source_interface') != \
-                element_change_check.get('spoke_ha_config', {}).get('source_interface') or \
-                elem_template.get('spoke_ha_config', {}).get('track', {}) != \
-                element_change_check.get('spoke_ha_config', {}).get('track', {}):
-            output_message("   Resetting Spoke HA in element {0}.".format(element_descriptive_text))
-            elem_template['spoke_ha_config'] = None
+    if reset_spoke_ha:
+        if spoke_ha_config and element_change_check.get('spoke_ha_config'):
+            if elem_template.get('spoke_ha_config', {}).get('source_interface') != \
+                    element_change_check.get('spoke_ha_config', {}).get('source_interface') or \
+                    elem_template.get('spoke_ha_config', {}).get('track', {}) != \
+                    element_change_check.get('spoke_ha_config', {}).get('track', {}):
+                output_message("   Resetting Spoke HA in element {0}.".format(element_descriptive_text))
+                elem_template['spoke_ha_config'] = None
+            else:
+                return 1
         else:
             return 1
     elif not force_update and elem_template == element_change_check:
